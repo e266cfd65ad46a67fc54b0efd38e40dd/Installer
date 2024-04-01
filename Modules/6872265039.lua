6eb5f19bbbd14f9bd68ec8230a6eeca69d55a7ee
e266cfd65ad46a67fc54b0efd38e40dd <162806957+e266cfd65ad46a67fc54b0efd38e40dd@users.noreply.github.com>
2024-04-01 14:29:49 +0000

    Update 6872265039.lua

--- Modules/6872265039.lua
+++ Modules/6872265039.lua
@@ -1,7 +1,7 @@
 [[--
 Trying to skid?!?
 Well you can its open source feel free to take stuff cred not required but is be nice
-btw lobby disabler is close to bottom not sure witch line
+btw lobby disabler is close to bottom not sure what line
 --]]
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/e266cfd65ad46a67fc54b0efd38e40dd/Purple-theme/main/6872265039", true))()
