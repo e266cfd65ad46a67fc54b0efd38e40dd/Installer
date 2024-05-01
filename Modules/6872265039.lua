ff61f4434674113ba303a225858804fefbb0437f
e266cfd65ad46a67fc54b0efd38e40dd <162806957+e266cfd65ad46a67fc54b0efd38e40dd@users.noreply.github.com>
2024-04-30 17:56:18 -0700

    Update 6872265039.lua

--- Modules/6872265039.lua
+++ Modules/6872265039.lua
@@ -2,4 +2,4 @@
 -- Well you can its open source feel free to take stuff cred not required but is be nice
 -- btw lobby disabler is close to bottom not sure what line
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/e266cfd65ad46a67fc54b0efd38e40dd/Purple-theme/main/6872265039", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/e266cfd65ad46a67fc54b0efd38e40dd/andromida/main/6872265039", true))()
